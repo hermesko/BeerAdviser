2b2df9595f4f1821a33d74a16a75e89f280ecd2c
Hermes Ko <hermesko@buffalo.edu>
2016-09-08 17:01:57 -0400

    Reverted Changes

--- app/src/main/java/com/hkktechnologies/beeradviser/BeerExpert.java
+++ app/src/main/java/com/hkktechnologies/beeradviser/BeerExpert.java
@@ -3,16 +3,12 @@ package com.hkktechnologies.beeradviser;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Hermes on 6/6/2016.
- */
 public class BeerExpert {
     List<String> getBrands(String color) {
         List<String> brands = new ArrayList<String>();
         if (color.equals("amber")) {
             brands.add("Jack Amber");
             brands.add("Red Moose");
-            brands.add("test");
         }
         else {
             brands.add("Jail Pale Ale");
